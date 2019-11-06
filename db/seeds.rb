0e70b99fa6f935803b91bb95ddef3db337c6d7c0
EC2 Default User <ec2-user@ip-172-31-88-23.ec2.internal>
2019-11-06 04:42:11 +0000

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+	{id: 1,
+		username: "Vu Huy Khoi",
+		email: "vuhuykhoi@gmail.com",
+		password: "123456", password_confirmation: "123456",
+		avatar: "/ITSS-pink/public/uploads/user/avatar/4/tải_xuống.jpg",
+	},
+	
+	{id: 2,
+		username: "Vu Tuan Anh",
+		email: "vutuananh@gmail.com",
+		password: "123456", password_confirmation: "123456",
+		avatar: "/ITSS-pink/public/uploads/user/avatar/4/tải_xuống.jpg",
+	},
+	
+	{id: 3,
+		username: "Pham Anh Hoang",
+		email: "phamanhhoang@gmail.com",
+		password: "123456", password_confirmation: "123456",
+		avatar: "/ITSS-pink/public/uploads/user/avatar/4/tải_xuống.jpg",
+	},
+	
+	{id: 4,
+		username: "Bui Xuan Que",
+		email: "buixuanque@gmail.com",
+		password: "123456", password_confirmation: "123456",
+		avatar: "/ITSS-pink/public/uploads/user/avatar/4/tải_xuống.jpg",
+	},
+	
+	{id: 5,
+		username: "Pham Van Thang",
+		email: "phamvanthang@gmail.com",
+		password: "123456", password_confirmation: "123456",
+		avatar: "/ITSS-pink/public/uploads/user/avatar/4/tải_xuống.jpg",
+	},
+])
+
+Provider.create!([
+	{id:1,
+		email: "ABC@gmail.com",
+		name: "ABC",
+		description: "",
+		address: "HaiBaTrung,HN",
+		phone_number: "0912345678",
+	},
+	
+	{id:2,
+		email: "AAA@gmail.com",
+		name: "AAA",
+		description: "",
+		address: "BachMai,HN",
+		phone_number: "0912345678",
+	},
+	
+	{id:3,
+		email: "BBB@gmail.com",
+		name: "BBB",
+		description: "",
+		address: "ThanhNhan,HN",
+		phone_number: "0912345678",
+	},
+	
+	{id:4,
+		email: "CCC@gmail.com",
+		name: "CCC",
+		description: "",
+		address: "BachKhoa,HN",
+		phone_number: "0912345678",
+	},
+])
